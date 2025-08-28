8c3dbf455d4ef8c140e364ddaeda6c0604f2eb51
Matheus Martins <mail@mmartins.dev>
2025-08-28 13:09:37 +0000

    feat: add postgres schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE accounts (
+      account_id BIGSERIAL PRIMARY KEY,
+      document_number VARCHAR(11) NOT NULL
+);
+
+CREATE TABLE transactions (
+      transaction_id BIGSERIAL PRIMARY KEY,
+      account_id BIGINT NOT NULL,
+      operation_type INT NOT NULL,
+      amount DECIMAL(15, 2) NOT NULL,
+      event_date TIMESTAMP NOT NULL DEFAULT NOW(),
+
+      CONSTRAINT fk_transaction_account FOREIGN KEY (account_id)
+          REFERENCES accounts(account_id)
+);
